5f4e413a7e5c5c406a65a7546905759c1cc2e91a
EdwardLiuyc <liuyongchuan@outlook.com>
2019-03-06 16:35:50 +0800

    add a cmake for eigen3 and its min version

--- cmake_modules/eigen.cmake
+++ cmake_modules/eigen.cmake
@@ -1,3 +1,4 @@
-find_package(Eigen3 REQUIRED)
-
+find_package(Eigen3 3.3.4 REQUIRED)
 include_directories(${EIGEN3_INCLUDE_DIR})
+message( STATUS 
+  "Found Eigen in dir: ${EIGEN3_INCLUDE_DIR} version: ${EIGEN3_VERSION}")
